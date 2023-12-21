777f9c263a245bb3a07b331db5f4bf85cc320f22
zebeneytnayn <116780207+zebeneytnayn@users.noreply.github.com>
2023-12-20 20:25:23 -0500

    Update search.py

--- phasebook/search.py
+++ phasebook/search.py
@@ -25,6 +25,27 @@ def search_users(args):
         a list of users that match the search parameters
     """
 
-    # Implement search here!
+    results = []
 
-    return USERS
+    for user in USERS:
+        if (
+            ("id" in args and args["id"] == user["id"]) or
+            ("name" in args and args["name"].lower() in user["name"].lower()) or
+            ("age" in args and user["age"] - 1 <= int(args["age"]) <= user["age"] + 1) or
+            ("occupation" in args and args["occupation"].lower() in user["occupation"].lower())
+        ):
+            results.append(user)
+
+    # remove id duplicate
+    unique_results = {user["id"]: user for user in results}.values()
+
+    # sorts
+    sorted_results = sorted(unique_results, key=lambda x: (
+        x["id"] == args.get("id", ""),
+        x["name"].lower() == args.get("name", "").lower(),
+        abs(x["age"] - int(args.get("age", 0))),
+        x["occupation"].lower() == args.get("occupation", "").lower()
+    ))
+
+    return list(sorted_results)
+
