befcb76d1b0b19228a94cee031350d9cdd79b48a
zebeneytnayn <116780207+zebeneytnayn@users.noreply.github.com>
2023-12-20 20:22:31 -0500

    Update match.py

--- phasebook/match.py
+++ phasebook/match.py
@@ -20,8 +20,6 @@ def match(match_id):
 
 
 def is_match(fave_numbers_1, fave_numbers_2):
-    for number in fave_numbers_2:
-        if number not in fave_numbers_1:
-            return False
-
-    return True
+    set_1 = set(fave_numbers_1)
+    set_2 = set(fave_numbers_2)
+    return set_2.issubset(set_1)
